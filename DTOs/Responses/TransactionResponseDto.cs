faa7b91e6f55127580901a966dc6c264a22ce1ea
Lucca Barboza <luccafrbz@gmail.com>
2026-02-07 21:45:28 -0300

    Criação do TransactionResponseDto

--- DTOs/Responses/TransactionResponseDto.cs
+++ DTOs/Responses/TransactionResponseDto.cs
@@ -1,8 +1,15 @@
+using FinanceApi.Models;
+
 namespace FinanceApi.DTOs.Responses
 {
     public class TransactionResponseDto
     {
         public Guid Id { get; set; }
-
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CompetenceDate { get; set; }
+        public DateTime LiquidationDate { get; set; }
+        public Guid AccountId { get; set; }
+        public TransactionType Type { get; set; }
     }
 }
