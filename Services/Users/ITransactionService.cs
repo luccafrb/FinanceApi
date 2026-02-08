a1a52e76312d04c81e0bcb2d12a5f0e1a7db5b73
Lucca Barboza <luccafrbz@gmail.com>
2026-02-07 21:54:29 -0300

    Ajuste na interface de transaction para retornar um Dto

--- Services/Users/ITransactionService.cs
+++ Services/Users/ITransactionService.cs
@@ -1,11 +1,11 @@
 using FinanceApi.DTOs.Create;
-using FinanceApi.Models;
+using FinanceApi.DTOs.Responses;
 
 namespace FinanceApi.Services.Users
 {
     public interface ITransactionService
     {
         public Task CreateAsync(TransactionCreateDto transactionCrateDto, Guid userId);
-        public Task<IEnumerable<Transaction>> GetAllAsync(Guid userId);
+        public Task<IEnumerable<TransactionResponseDto>> GetAllAsync(Guid userId);
     }
 }
